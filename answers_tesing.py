8495b0f63ccb11af993fcf7739322e3ad9eddc49
nichnikov <a.nichnikov@gmail.com>
2023-07-18 21:30:46 +0300

    script refactor

--- answers_tesing.py
+++ answers_tesing.py
@@ -17,17 +17,13 @@ results_df = pd.DataFrame()
 while sign:
     parameters="pageSize={}&pageNumber={}&period={}&sys={}".format(str(pageSize), str(pageNumber), str(period), str(sys))
     url = "".join([URL, parameters])
-
     response = requests.get(url)
-    qrs_df = pd.DataFrame(response.json())
-    results_df = pd.concat([results_df, qrs_df])
 
-    if response.json(): 
+    if response.json():
+        qrs_df = pd.DataFrame(response.json())
+        results_df = pd.concat([results_df, qrs_df]) 
         pageNumber += 1    
     else:
         sign = False
     
-    print(pageNumber)
-    print(results_df)
-
 results_df.to_csv(os.path.join("data", "queries.csv"), sep="\t", index=False)
